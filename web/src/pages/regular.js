e8e4d8d7d1209d6ccb873d77e9618f7c48163e3b
yfsoftcom <yfsoftcom@126.com>
2021-03-10 08:21:52 +0800

    fix about the reg style

--- web/src/pages/regular.js
+++ web/src/pages/regular.js
@@ -8,11 +8,12 @@ export default () => {
   return (
     <div>
       <div className="input-group box">
-        <p className="label item">Regular Express:</p> <input className="input item" />
+        <p className="label item">Regular:</p> 
+        <input className="input item" placeholder="/^http$/gi"/>
       </div>
       <div className="input-group box">
         <p className="label item">Text: </p>
-        <textarea className="input text item">
+        <textarea className="input text item" rows="10">
         </textarea>
       </div>
     </div>
