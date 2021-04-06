9b527e5e06d0ea2a54756c24251ec98790d70919
yfsoftcom <yfsoftcom@126.com>
2021-04-06 22:30:12 +0800

    give a shit

--- web/src/app.js
+++ web/src/app.js
@@ -4,7 +4,7 @@ export default (props) => {
   return (
     <div>
       <div className="header"></div>
-      <h1 className="title"> ⚒  🔩 ⚙ ⛓ 🔧  🔨 Coder Kits (version: beta.01)</h1>
+      <h1 className="title"> ⚒  🔩 ⚙ ⛓ 🔧  🔨 Coder Kits (version: beta.01.1)</h1>
       <div className="container">
         <div className="">
           <div className="margin-top-2"></div>
