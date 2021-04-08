f186ee94f67ca1444a2b6f882f2e0fbba2354a0c
yfsoftcom <yfsoftcom@126.com>
2021-04-08 22:13:54 +0800

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+# Set the required provider and versions
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.11.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "ssh://root@hk.yunplus.io"
+}
+
+# Create a container
+resource "docker_container" "coder-kit" {
+  image = docker_image.coder-kit.latest
+  name  = "coder-kit"
+  restart = "always"
+  must_run = true
+  ports {
+     internal = 80
+     external = 18080
+  }
+}
+
+data "docker_registry_image" "coder-kit" {
+  name = "yfsoftcom/coder-kit:latest"
+}
+
+resource "docker_image" "coder-kit" {
+  name          = data.docker_registry_image.coder-kit.name
+  pull_triggers = [data.docker_registry_image.coder-kit.sha256_digest]
+  force_remove = true
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "docker-image" {
+  type        = string
+  description = "name of the docker image to deploy"
+  default     = "yfsoftcom/coder-kit:latest"
+}
