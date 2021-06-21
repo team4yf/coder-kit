dc44c75ea6c440fc73eef800cdc9258399266aff
yfsoftcom <yfsoftcom@126.com>
2021-06-21 20:04:18 +0800

    add env

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -7,7 +7,7 @@ export default () => {
       <h2 className="title"> 🐢 About Me. 🐢 </h2>
       <div className="title-line"></div>
       <p>I'm a coder from JiangSu, happy hacking!</p>
-      <p> Version: 0.1.0.2 </p>
+      <p> Version: 0.1.0.3 </p>
     </div>
   )
 };
