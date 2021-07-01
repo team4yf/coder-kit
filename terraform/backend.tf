fa2336adfc3b6b0f59bbbf4a0841c3e8725ca7fc
MR.Fan <yfsoftcom@gmail.com>
2021-07-01 16:24:59 +0800

    try to run ci

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "yunplus-terraform"
-    key    = "io/yunplus/coder-kit"
-    region = "us-east-1"
-  }
-}
--- terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "docker-image" {
-  type        = string
-  description = "name of the docker image to deploy"
-  default     = "yfsoftcom/coder-kit:latest"
-}
